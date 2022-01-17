1024da371c9e6eabf77d8187ec4e805a062e5c29
ChrisGuy <chrismorgan1526@gmail.com>
2022-01-17 14:04:54 +0100

    Initial set up of widget, GET request to HFL app to receive games

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isLoading, setIsLoading] = useState(false);
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		axios
+			.get(`/games.json`)
+			.then(setIsLoading(true))
+			.then((res) => {
+				setData(res.data);
+				setIsLoading(false);
+			});
+	}, []);
+	console.log(data);
+	return (
+		<div className='App'>
+			{isLoading ? <h1>Loading...</h1> : <h1>Data Received</h1>}
+		</div>
+	);
 }
 
 export default App;
